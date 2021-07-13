b9f5aa0aab71b7a2231116500dede07e934875e5
cllcnkrt <cllcnkrt@outlook.com>
2021-07-13 14:52:42 +0300

    project completed

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,31 +1,57 @@
-import React from 'react'
-import Book from './Book'
-import './BookList.css'
+import React from 'react';
+import './BookList.css';
+import Book from './Book';
+import  { BookContext } from '../contexts/BookContext';
+import  { ThemeContext } from '../contexts/ThemeContext';
+
+class BookList extends React.Component {
 
 
-function BookList(props) {
-    
-    const bookList = props.books.map((book, i) =>{
-        return <Book book={book} key ={i}/>
-    })
-    
-    return (
+    render() {
+        return (
 
-        <section className="page-section" id="portfolio">
-        <div className="container">
-            <div className="text-center">
-                <h2 className="section-heading text-uppercase">BookFolio</h2>
-                <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
-               
-            </div>
-            <div className="row">
+            <ThemeContext.Consumer>{(contextTheme) => (
+                <BookContext.Consumer>
+                    {contextBook => {
+                    
+                    const { books } = contextBook;
+                    const {changeTheme, isDarkTheme, dark, light } = contextTheme;
+                    const theme = isDarkTheme ? dark : light;
+                
+                    return (
+                        <section className="page-section" style={{background: theme.bg, color:theme.txt}} id="portfolio">
+                        <div className="container">
+                            <div className="text-center">
+                                <h2 className="section-heading text-uppercase">BookFolio</h2>
+                                <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                                <button type="button" className="btn btn-sm btn-info" style={{marginTop: '-70px'}}
+                                onClick={changeTheme}>Change Theme</button>
+                            </div>
+                            <div className="row">
+        
+                                {books.map((book, i) => {
+                                    return <Book book={book}
+                                        key={i}
+                                    />
+                                })}
+        
+                            </div>
+                        </div>
+                    </section>
+                    )
+                }}
+            </BookContext.Consumer>
+            )}
 
-                {bookList}
 
-            </div>
-        </div>
-    </section>
-    )
+            </ThemeContext.Consumer>
+
+
+
+
+        )
+    }
+
 }
 
 export default BookList
